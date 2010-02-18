c4a13f2ce3a0d8f381fa6927b01cf54042772268
Azhaguselvan <azhaguselvan@gmx.com>
2010-02-19 00:39:04 +0530

    Removed JSON dependency - converted to pure python

---
+++ twitter/sample_authinfo.py
@@ -0,0 +1,10 @@
+auth_dict = {
+    "auth_dict": {
+        "user": "gate_way_acc_which_you_need_to_send_DM", 
+        "pass": "gate_way_acc_pass", 
+        "su-scren_name": "screen_name_of_user_who_can_execute_as_super_user", 
+        "su-pass": "sudo_pass", 
+        "su-id": 22607833 #The screen id of user who can execute as super user
+    }, 
+    "last_exec_id": 819589043
+}
