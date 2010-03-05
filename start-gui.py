48b167dd59e35e5a1fdf52e3a51eddda0f6c6ea1
Azhaguselvan <azhaguselvan@gmx.com>
2010-03-05 23:28:11 +0530

    minor changes in gui

--- start-gui.py
+++ start-gui.py
@@ -10,20 +10,19 @@ class App:
         self.button = Button(frame, text="QUIT", fg="red", command=frame.quit)
         self.button.pack(side=LEFT)
 
-        self.hi_there = Button(frame, text="Run SMS Deamon", command=self.run_sms)
-        self.hi_there.pack(side=LEFT)
+        self.run_sms = Button(frame, text="Run SMS Deamon", command=self.run_sms)
+        self.run_sms.pack(side=LEFT)
 
-        self.hi_there = Button(frame, text="Run Twitter Deamon", command=self.run_twitter)
-        self.hi_there.pack(side=LEFT)
+        self.run_twitter = Button(frame, text="Run Twitter Deamon", command=self.run_twitter)
+        self.run_twitter.pack(side=LEFT)
 
-    def say_hi(self):
-        print "hi there, everyone!"
-
+          
     def run_sms(self):
         p1 = Popen(['python sms/console.py'],shell=True)
         
     def run_twitter(self):
         p2 = Popen(['python twitter/console.py'],shell=True)
+
 
 root = Tk()
 
